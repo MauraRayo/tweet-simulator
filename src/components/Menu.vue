22af00679f307c4967fdfd046ce4d273d29a9603
MauraRayo <rayomaura@gmail.com>
2024-01-03 16:48:55 +0100

    creacion de Menu y TweetForm

---
+++ src/components/Menu.vue
@@ -0,0 +1,21 @@
+
+<template>
+ <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <span class="navbar-brand">Simulador de tweet</span>
+        <button class="btn pl-4 pr-4 btn-success">Nuevo tweet</button>
+    </div>
+ </nav>
+
+ 
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/TweetForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="tweet-form container">
+        <form>
+            <input class="form-control" type="text" placeholder="Tu nombre">
+            <textarea class="form-control"  rows="3" placeholder="Escribe tu tweet"></textarea>
+            <button type="button" class="btn btn-primary">Enviar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.tweet-form{
+    margin-top: 20px;
+    // height:0;
+    overflow:  hidden;
+
+    form{
+        margin-bottom: 50px;
+    }
+    input {
+        margin-bottom: 10px;
+
+    }
+    button {
+        width: 100%;
+        margin-top: 10px ;
+    }
+}
+
+</style>
